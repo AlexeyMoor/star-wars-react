9957cc80eeaf85c6380e31e324ffaf4405a7ee20
Alexey Moor <asmodeus.1819@gmail.com>
2025-11-04 12:58:34 +0100

    Add dynamic fetching of opening crawl and Star Wars information

--- src/components/OpeningCrawl.jsx
+++ src/components/OpeningCrawl.jsx
@@ -1,9 +1,30 @@
-import {openingCrawl} from "../utils/constants.js";
+import {base_url} from "../utils/constants.js";
+import {useEffect, useState} from "react";
+
 
 const OpeningCrawl = () => {
-  return (
-    <p className="far-galaxy fs-2 lh-2">{openingCrawl}</p>
-  );
+  const [openingCrawl, setOpeningCrawl] = useState();
+  useEffect(() => {
+    const episode = Math.floor(Math.random() * 6) + 1;
+    fetch(`${base_url}/v1/films/${episode}`)
+      .then(response => response.json())
+      .then(data => setOpeningCrawl(data.opening_crawl))
+      .catch(() => setOpeningCrawl('Error on fetching opening crawl.'));
+    // return () => console.log('component OpeningCrawl unmounted');
+  }, []);
+
+  if (openingCrawl) {
+    return (
+      <p className="far-galaxy fs-2 lh-2">{openingCrawl}</p>
+    );
+  } else {
+    return (
+      <p className="far-galaxy fs-2 lh-2">
+        <span className={'spinner-border spinner-border-sm'}></span>
+        <span className={'spinner-grow spinner-border-sm'}> Loading...</span>
+      </p>
+    );
+  }
 };
 
 export default OpeningCrawl;
